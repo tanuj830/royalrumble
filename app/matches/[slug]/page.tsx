47515cb070a0283c5d3058376ca44f3ee1db3579
tanuj <tanujbhatt644@gmail.com>
2024-03-16 20:53:35 +0530

    carousel added

--- app/matches/[slug]/page.tsx
+++ app/matches/[slug]/page.tsx
@@ -1,10 +1,36 @@
 "use client";
 import { usePathname } from "next/navigation";
 import React from "react";
+import { fights } from "@/constants/matches";
+import { Button } from "@/components/ui/button";
 
-const Match = () => {
-  const pathname = usePathname();
-  return <div>Match: Exploring particular fight with id= {pathname}</div>;
+const Match = ({ params }: any) => {
+  const fight: any = fights.filter((f) => {
+    return f.id === params.slug;
+  });
+  const match = fight[0];
+  return (
+    <div>
+      {match ? (
+        <div>
+          <div className="relative">
+            <img
+              className="shadow-lg shadow-muted fade "
+              src={match.img}
+              alt=""
+            />
+            <div className="absolute bottom-4 w-full flex justify-center items-center ">
+              <Button>Scheduled on 16 Mar 2024</Button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="flex justify-center h-full w-full items-center">
+          <span>Nothing to show</span>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Match;
