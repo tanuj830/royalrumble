6475cc566ee0be51672621ecd15088e6262eb0a4
tanuj <tanujbhatt644@gmail.com>
2024-03-16 21:55:18 +0530

    carousel added

--- constants/matches.ts
+++ constants/matches.ts
@@ -27,22 +27,21 @@ export const fights = [
       title: "Manny Pacquioa vs Errol Spence Jr| Boxing Match",
       img: "https://img.freepik.com/premium-psd/mma-fight-sport-flyer-template_524343-457.jpg?size=626&ext=jpg&ga=GA1.1.630556571.1709205587&semt=ais",
       disp:"  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem alias aperiam laborum minima commodi fugiat nostrum dolor esse? Rerum corporis repudiandae amet eum eos excepturi cupiditate molestias, esse culpa nemo illum deserunt accusantium voluptates fuga quis labore. Harum cupiditate inventore similique fugiat hic. Laudantium nemo nesciunt unde quos fugit. Reprehenderit rerum eos esse minus officia libero beatae soluta eius quaerat error facere quae nihil vitae odio porro architecto similique itaque hic qui in doloremque, fugiat quisquam! Ab qui hic saepe corporis, laboriosam exercitationem, vel, assumenda placeat similique voluptatem aliquam quasi ducimus sint facere fuga eos maxime deserunt alias cupiditate necessitatibus.",
-      teams:
-      [
-        {
-            player:
-            {
-                name:"Manny Pacquioa",
-
-            },
-        },
-        {
-            player:
-            {
-                name:"Errol Spence",
-                
-            },
-        }
-      ]
+      teamA:[
+        {       player:
+               {
+                   name:"Manny Pacquioa",
+   
+               },
+       }],
+           teamB:[
+   {
+               player:
+               {
+                   name:"Errol Spence",
+                   
+               },
+              }       ]
+         
     },
   ];
